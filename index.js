fd36d4dfa22a7486aefe685cca862cf1ce080d69
yousufali <yousaf.syed5@gmail.com>
2020-09-17 06:44:32 +0500

    Part3_adding-Eslint + backend deploy stage with mongodb

--- index.js
+++ index.js
@@ -5,65 +5,67 @@ const morgan = require('morgan')
 const cors = require('cors')
 
 const Note = require('./models/note')
-const { update } = require('./models/note')
 
 
 const app = express()
 
 app.use(cors())
 app.use(express.static('build'))
-morgan.token("mylooger", req => JSON.stringify(req.body))
+morgan.token('mylooger', req => JSON.stringify(req.body))
 
 app.use(morgan(':method :url :status :response-time :mylooger'))
 app.use(express.json())
 
 const wrongurl = (req, res) => {
-  return res.status(404).send("Unknown address")
+    return res.status(404).send('Unknown address')
 }
 
 const errorHandler = (error, req, res, next) => {
-  console.log(error.message)
+    console.log(error.message)
 
-  if (error.name === 'CastError') {
-    return res.status(400).send({ error: 'malformed id' })
-  }
-  next(error)
+    if (error.name === 'CastError') {
+        return res.status(400).send({ error: 'malformed id' })
+    }else if(error.name === 'ValidationError'){
+        return res.status(400).json({ error: error.message })
+
+    }
+    next(error)
 }
 
 app.use(errorHandler)
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2020-01-10T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2020-01-10T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2020-01-10T19:20:14.298Z",
-    important: true
-  }
-]
+// let notes = [
+//   {
+//     id: 1,
+//     content: 'HTML is easy',
+//     date: '2020-01-10T17:30:31.098Z',
+//     important: true
+//   },
+//   {
+//     id: 2,
+//     content: 'Browser can execute only Javascript',
+//     date: '2020-01-10T18:39:34.091Z',
+//     important: false
+//   },
+//   {
+//     id: 3,
+//     content: 'GET and POST are the most important methods of HTTP protocol',
+//     date: '2020-01-10T19:20:14.298Z',
+//     important: true
+//   }
+// ]
 
 app.get('/', (req, res) => {
-  //res.send('<h1>Hello Yousuf!     base address render</h1>')
-  res.sendFile('index.html')
+    //res.send('<h1>Hello Yousuf!     base address render</h1>')
+    res.sendFile('index.html')
 
 })
 
 app.get('/api/notes', (req, res) => {
-  // res.json(notes)
-  Note.find({}).then(result => {
-    res.json(result)
-  })
+    // res.json(notes)
+    Note.find({}).then(result => {
+        res.json(result)
+    })
 })
 
 // const generateId = () => {
@@ -73,89 +75,93 @@ app.get('/api/notes', (req, res) => {
 //   return maxId + 1
 // }
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body
+app.post('/api/notes', (request, response,next) => {
+    const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
-  }
+    // if (!body.content) {
+    //   return response.status(400).json({
+    //     error: 'content missing'
+    //   })
+    // }
 
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
+    const note = new Note({
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
     // id: generateId(),
-  })
+    })
 
-  // notes = notes.concat(note)
+    // notes = notes.concat(note)
 
-  // response.json(note)
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
+    // response.json(note)
+    note.save()
+        .then(resultnote => resultnote.toJSON())
+        .then(savedNote => {
+            // console.log('savednote::::',savedNote)
+            response.json(savedNote)
+        })
+        .catch(error => next(error))
 })
 
 app.get('/api/notes/:id', (request, response, next) => {
 
-  Note.findById(request.params.id)
-    .then(note => {
-      if (note) {
-        response.json(note)
-      } else {
-        response.status(404).end()
-      }
-    }
-    )
-    .catch(error => {
-      // console.log(',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,')
-      // console.log(error)
-      // console.log('..........................................')
+    Note.findById(request.params.id)
+        .then(note => {
+            if (note) {
+                response.json(note)
+            } else {
+                response.status(404).end()
+            }
+        }
+        )
+        .catch(error => {
+            // console.log(',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,')
+            // console.log(error)
+            // console.log('..........................................')
 
-      // response.status(400).send({error:'malformated id'})
-      next(error)
-    })
+            // response.status(400).send({error:'malformated id'})
+            next(error)
+        })
 
-  // const id = Number(request.params.id)
-  // const note = notes.find(note => note.id === id)
-  // if (note) {
-  //   response.json(note)
-  // } else {
-  //   response.status(404).end()
-  // }
+    // const id = Number(request.params.id)
+    // const note = notes.find(note => note.id === id)
+    // if (note) {
+    //   response.json(note)
+    // } else {
+    //   response.status(404).end()
+    // }
 })
 
 app.delete('/api/notes/:id', (request, response, next) => {
-  // const id = Number(request.params.id)
-  // notes = notes.filter(note => note.id !== id)
+    // const id = Number(request.params.id)
+    // notes = notes.filter(note => note.id !== id)
 
-  // response.status(204).end()
-  Note.findByIdAndRemove(request.params.id)
-    .then(result => response.status(204).end())
-    .catch(error => next(error))
+    // response.status(204).end()
+    Note.findByIdAndRemove(request.params.id)
+        .then(result => response.status(204).end())
+        .catch(error => next(error))
 
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
+    const body = request.body
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then(updateNote => {
-      console.log(updateNote)
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+        .then(updateNote => {
+            console.log(updateNote)
 
-      response.json(updateNote)
-    })
-    .catch(error => next(error))
+            response.json(updateNote)
+        })
+        .catch(error => next(error))
 })
 
 app.use(wrongurl)
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
--- models/note.js
+++ models/note.js
@@ -4,22 +4,29 @@ const mongoose = require('mongoose')
 // const url = `mongodb+srv://fsdev:asd@cluster0.n829h.mongodb.net/note-app?retryWrites=true&w=majority`
 const url = process.env.MONGODB_URI
 
-console.log("connecting to url", url)
+console.log('connecting to url', url)
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(result=>
+    .then(result =>
     {
-        console.log("connected to mongodb")
+        console.log('connected to mongodb')
     })
-.catch((error)=>{
-    console.log('error connecting to mongodb ',error.message)
-})
+    .catch((error) => {
+        console.log('error connecting to mongodb ',error.message)
+    })
 
 
 const noteSchema = new mongoose.Schema(
     {
-        content: String,
-        date: Date,
+        content: {
+            type:String,
+            minlength: 5,
+            required:  true
+        },
+        date: {
+            type:Date,
+            required: true
+        },
         important: Boolean,
     }
 )
