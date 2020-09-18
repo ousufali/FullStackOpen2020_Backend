d294b0564fa3d1df9658305b4bd4458855bc8538
yousufali <yousaf.syed5@gmail.com>
2020-09-18 16:44:15 +0500

    Part4::Project structure_TheNoteApp

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const notesRouter = require('./controllers/notes')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+
+
+logger.info('Connecting to ', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => logger.info('Connected to MongoDB'))
+    .catch((error) => logger.error('Error connection to MongoDB;', error.message))
+
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use('/api/notes', notesRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
--- models/note.js
+++ models/note.js
@@ -1,20 +1,6 @@
 const mongoose = require('mongoose')
 
-
-// const url = `mongodb+srv://fsdev:asd@cluster0.n829h.mongodb.net/note-app?retryWrites=true&w=majority`
-const url = process.env.MONGODB_URI
-
-console.log('connecting to url', url)
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(result =>
-    {
-        console.log('connected to mongodb')
-    })
-    .catch((error) => {
-        console.log('error connecting to mongodb ',error.message)
-    })
-
+mongoose.set('useFindAndModify' , false)
 
 const noteSchema = new mongoose.Schema(
     {
